9f2d8b65b6ec2f2ed924db6936a5b1a2962dd454
ericmeneg <ericvinicius.menegon@gmail.com>
2024-04-20 16:48:47 -0300

    Otimização de "pokemon_encounter_simulator.py" por meio da substituição de lógica baseada em random.randrange() pelo uso dos métodos random.choice() e random.choices()

--- python/pokemon_encounter_simulator.py
+++ python/pokemon_encounter_simulator.py
@@ -2,48 +2,13 @@
 #This code simulates the effects of the algorythm used in the pokemon ultra sun and pokemon ultra moon games to determine random encounters in mount hokulani's first two fields at the foot of the mountain during the day, as listed on bulbapedia
 import random
 def species_det():
-    species_check = random.randrange(0,101)
-    held_item_type = 0
-    held_item_odds = 0 
-    held_item_type_two = 0
-    held_item_odds_two = 0
-    if species_check <=30:
-        species = "Fearow"
-        genderless = False
-        held_item_odds = 5
-        held_item_type = "Sharp Beak"
-    elif 40>=species_check>30:
-        species = "Ditto"
-        held_item_odds = 5
-        held_item_odds_two = 55
-        held_item_type = "Metal Powder"
-        held_item_type_two = "Quick Powder"
-        genderless = True
-    elif 50>=species_check>40:
-        species = "Skarmory"
-        held_item_odds = 5
-        held_item_type = "Metal Coat"
-        genderless = False
-    elif 60>=species_check>50:
-        species = "Elekid"
-        held_item_odds = 5
-        held_item_type = "Electririzer"
-        genderless = False
-    elif 70>=species_check>60:
-        held_item_odds = 5
-        held_item_type = "Metal Coat"
-        species = "Beldum"
-        genderless = True
-    elif 90>=species_check>70:
-        species = "Elgyem"
-        held_item_odds = 0
-        held_item_type = None
-        genderless = False
-    elif 100>=species_check>90:
-        species = "Minior"
+    genderless = False
+    species_pool = ["Fearow","Ditto","Skarmory","Elekid","Beldum","Elgyem","Minior"]
+    species = random.choices(species_pool, weights=(30,10,10,10,10,20,10), k=1)[0]
+    if species == "Ditto" or species == "Beldum" or species == "Minior":
         genderless = True
     print(species)
-    return genderless,held_item_odds,held_item_type,held_item_odds_two,held_item_type_two,species
+    return species,genderless
 
 def ability_def(species):
     ability_check = random.randrange(1,3)
@@ -68,38 +33,35 @@ def ability_def(species):
     print("It's ability is",ability)
 
 def form_det():
-    color = random.randrange(1,7)
-    if color == 1:
-        color = "red"
-    if color == 2:
-        color = "orange"
-    if color == 3:
-        color = "yellow"
-    if color == 4:
-        color = "green"
-    if color == 5:
-        color = "blue"
-    if color == 6:
-        color = "violet"
+    color_pool = ["red","orange","yellow","green","blue","violet"]
+    color = random.choice(color_pool)
     print("It has a",color,"core")
 
-def held_item(held_item_odds,held_item_type,held_item_odds_two,held_item_type_two):
-    check = random.randrange(1,101)
-    if check <= held_item_odds:
-        print("The pokemon is holding a",held_item_type)
-    elif check <= held_item_odds_two and held_item_odds_two != 0:
-        print("The pokemon is holding a",held_item_type_two)
+def held_item(species):
+    itemcheck = random.randrange(1,101)
+    item = None
+    if species == "Fearow" and itemcheck<=5:
+        item = "Sharp Beak"
+    if species == "Ditto":
+        itempool = ["Quick Powder","Metal Powder", None]
+        item = random.choices(itempool, weights=(50,5,45))
+    if species == "Skarmory" and itemcheck<=5:
+        item = "Metal Coat"
+    if species == "Elekid" and itemcheck<=5:
+        item = "Electirizer"
+    if species == "Beldum" and itemcheck<=5:
+        item = "Metal Coat"
+    if species =="Minior" and itemcheck<=5:
+        item = "Star Piece"
+    if item != None:
+        print("It's holding a",item)
 
 def gender_det(genderless):
     if genderless == True:
-        gender = "Genderless"
+        print("Genderless")
     else:
-        gender = random.randrange(1,3)
-        if gender == 1:
-            gender = "M"
-        else:
-            gender = "F"
-    print(gender)
+        genderpool = ["M","F"]
+        print(random.choice(genderpool))
 
 def level_det():
     print("Level",random.randrange(27,31))
@@ -119,28 +81,17 @@ def shininess_check():
     return shiny
 
 def nature_det():
-    affected_stat = random.randrange(1,7)
-    if affected_stat == 1:
-        affected_stat = "HP"
-    elif affected_stat == 2:
-        affected_stat = "Atk"
-    elif affected_stat == 3:
-        affected_stat = "Def"
-    elif affected_stat == 4:
-        affected_stat = "Sp. Atk"
-    elif affected_stat == 5:
-        affected_stat = "Sp. Def"
-    elif affected_stat == 6:
-        affected_stat = "Spe"
+    stats = ["HP","Atk","Def","Sp. Atk","Sp. Def","Spe"]
+    affected_stat = random.choice(stats)
     return affected_stat
 
-'''genderless,held_item_odds,held_item_type,held_item_odds_two,held_item_type_two,species = species_det()
+'''species,genderless = species_det()
 level_det()
 gender_det(genderless)
 ability_def(species)
 if species == "Minior":
     form_det()
-held_item(held_item_odds,held_item_type,held_item_odds_two,held_item_type_two)
+held_item(species)
 shininess_check()
 iv_gen("HP")
 iv_gen("Atk")
@@ -159,13 +110,13 @@ else:
 count = 0
 while True:
     count += 1
-    genderless,held_item_odds,held_item_type,held_item_odds_two,held_item_type_two,species = species_det()
+    species,genderless = species_det()
     level_det()
     gender_det(genderless)
     ability_def(species)
     if species == "Minior":
         form_det()
-    held_item(held_item_odds,held_item_type,held_item_odds_two,held_item_type_two)
+    held_item(species)
     shiny = shininess_check()
     iv_gen("HP")
     iv_gen("Atk")
